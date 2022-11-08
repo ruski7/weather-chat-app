1e33cb008754d103a32055fa95fe38e1262ef515
Showmik Roy <16091118+showmikr@users.noreply.github.com>
2022-11-08 05:44:02 -0800

    Implemented User Sign In

--- app/src/main/java/com/example/team_7_tcss_450/ui/HomeFragment.java
+++ app/src/main/java/com/example/team_7_tcss_450/ui/HomeFragment.java
@@ -2,23 +2,42 @@ package com.example.team_7_tcss_450.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.team_7_tcss_450.R;
+import com.example.team_7_tcss_450.databinding.FragmentHomeBinding;
+import com.example.team_7_tcss_450.model.UserInfoViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
 
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        UserInfoViewModel userInfoModel = new ViewModelProvider(requireActivity())
+                .get(UserInfoViewModel.class);
+
+        FragmentHomeBinding.bind(requireView()).editHome.setText(
+                getResources().getString(R.string.home_greeting, userInfoModel.getEmail()));
     }
 }
