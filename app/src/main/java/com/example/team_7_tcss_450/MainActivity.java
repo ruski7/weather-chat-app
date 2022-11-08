f1635d14e234dd88720c0f66909799b4d4a99c31
David Shcherbina <dsns@uw.edu>
2022-11-08 01:07:20 -0800

    Bottom Navigation Bar added
    -includes a theme in themes.xml for a view that doesn't include the bar for testing (auto generated).
    
    -currently displays fragment text in nav bar for only the one currently in view.
    
    -added fragment AccountFragment.java
    
    -navbar fragments to include home -> weather -> messages -> account.

--- app/src/main/java/com/example/team_7_tcss_450/MainActivity.java
+++ app/src/main/java/com/example/team_7_tcss_450/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.team_7_tcss_450;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        // Make sure the new statements go BELOW setContentView
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_weather, R.id.navigation_message, R.id.navigation_account)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
+
 }
