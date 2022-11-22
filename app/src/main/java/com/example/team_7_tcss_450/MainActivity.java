c195ac74ff0ac8646eef7c883bd1e1539a50cf4f
David Shcherbina <dsns@uw.edu>
2022-11-22 11:09:47 -0800

    launch off point (temp changes)
    - Switching development environments
    - No updates

--- app/src/main/java/com/example/team_7_tcss_450/MainActivity.java
+++ app/src/main/java/com/example/team_7_tcss_450/MainActivity.java
@@ -9,20 +9,21 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.team_7_tcss_450.model.UserInfoViewModel;
 import com.example.team_7_tcss_450.ui.account.AccountViewModel;
+import com.example.team_7_tcss_450.utils.Utils;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration mAppBarConfiguration;
 
     private AccountViewModel accountModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(this);
 
         MainActivityArgs args = MainActivityArgs.fromBundle(getIntent().getExtras());
 
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         ).get(UserInfoViewModel.class);
 
         setContentView(R.layout.activity_main);
+
         // Make sure the new statements go BELOW setContentView
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -45,17 +47,61 @@ public class MainActivity extends AppCompatActivity {
                 setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
+        // TODO: FIX DARK MODE TOGGLE - CURRENTLY BROKEN
         // Account Fragment Binding
         accountModel = new ViewModelProvider(this).get(AccountViewModel.class);
-        accountModel.getStatus().observe(this, Switch -> {
+        accountModel.getStatus().observe(this, SwitchMaterial -> {
+
             // Perform an action with the latest item data
-            if(accountModel.getStatus().getValue()){
+            if(Boolean.TRUE.equals(accountModel.getStatus().getValue())){
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                Log.d("Weather App", "Switch Gets Val \"ON\"");
             } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                Log.d("Weather App", "Switch Gets Val \"OFF\"");
             }
         });
 
+
+        // Theme Setting - Allowing dynamic changing of app theme
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Weather App","Application Start");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Weather App", "Application Restart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Weather App","Application Resume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Weather App", "Application Pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Weather App","Application Stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Weather App","Application Destroy");
     }
 
 }
--- app/src/main/java/com/example/team_7_tcss_450/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/team_7_tcss_450/ui/account/AccountFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.team_7_tcss_450.databinding.FragmentAccountBinding;
+import com.example.team_7_tcss_450.ui.register.RegistrationViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,10 +24,18 @@ public class AccountFragment extends Fragment {
     private FragmentAccountBinding binding;
     private AccountViewModel mAccountModel;
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAccountModel = new ViewModelProvider(requireActivity())
+                .get(AccountViewModel.class);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
+//          Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_account, container, false);
 
         binding = FragmentAccountBinding.inflate(inflater);
@@ -35,12 +45,30 @@ public class AccountFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mAccountModel = new ViewModelProvider(requireActivity()).get(AccountViewModel.class);
+
+
+        // Action Listener - Theme Color Change
+        binding.toggleTheme.setOnClickListener(SwitchMaterial -> {
+
+        });
+
+
 
         // Toggles Dark Mode on click (changes view model boolean)
-        binding.toggleThemeSwitch.setOnClickListener(item -> {
-            Log.d("Weather App", "Toggling Dark Mode -> " + binding.toggleThemeSwitch.isChecked());
-            mAccountModel.selectState(binding.toggleThemeSwitch.isChecked());
+        binding.toggleDarkMode.setOnClickListener(SwitchMaterial -> {
+            Log.d("Weather App", "Toggling Dark Mode -> " + binding.toggleDarkMode.isChecked());
+
+            // Sets the State for Toggle that way MainActivity can view and react to changes
+            mAccountModel.selectState(binding.toggleDarkMode.isChecked());
+
+            //Boolean.TRUE.equals(mAccountModel.getStatus().getValue())
+            if(binding.toggleDarkMode.isChecked()) {
+//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                Log.d("Weather App", "Fragment Dark Mode -> ON");
+            } else {
+//                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                Log.d("Weather App", "Fragment Dark Mode -> OFF");
+            }
         });
 
     }
