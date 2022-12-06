668d92ec7448933b0de12948ffe1f77b35c02318
andrewUW <ajn26@uw.edu>
2022-12-06 13:41:17 -0800

    Update Contacts

--- app/src/main/java/com/example/team_7_tcss_450/ui/contacts/Contact.java
+++ app/src/main/java/com/example/team_7_tcss_450/ui/contacts/Contact.java
@@ -3,26 +3,62 @@ package com.example.team_7_tcss_450.ui.contacts;
 
 public class Contact {
 
-    private final String firstName;
-    private final String lastName;
-    private final String contactNumber;
+    private final String mUserName;
+    private final String mFirstName;
+    private final String mLastName;
+    private final String mEmail;
+    private final int memberID;
 
-    public Contact(String firstName, String lastName, String contactNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.contactNumber = contactNumber;
+    /**
+     * Represents a contact.
+     * @param userName
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param memID
+     */
+    public Contact(String userName, String firstName, String lastName, String email, int memID) {
+        this.mUserName = userName;
+        this.mFirstName = firstName;
+        this.mLastName = lastName;
+        this.mEmail = email;
+        this.memberID = memID;
     }
 
+    /**
+     * Gets contact username.
+     * @return username.
+     */
+    public String getUserName() { return mUserName; }
+
+    /**
+     * Gets contact first name.
+     * @return first name.
+     */
     public String getFirstName() {
-        return firstName;
+        return mFirstName;
     }
 
+    /**
+     * Gets contact last name.
+     * @return last name.
+     */
     public String getLastName() {
-        return lastName;
+        return mLastName;
     }
 
-    public String getContactNumber() {
-        return contactNumber;
+    /**
+     * Gets contact email.
+     * @return email.
+     */
+    public String getEmail() { return mEmail; }
+
+    /**
+     * Gets contact member id.
+     * @return member id.
+     */
+    public int getMemberID() {
+        return memberID;
     }
 }
 
