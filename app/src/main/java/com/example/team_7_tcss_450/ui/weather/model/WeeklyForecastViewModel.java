fba2f8dfd4e46cced5820ef53e77c6f088c18bb7
Rhanson7 <rhanson7@uw.edu>
2022-12-11 16:01:40 -0800

    Added hourly & current forecast view models and data objects

--- app/src/main/java/com/example/team_7_tcss_450/ui/weather/model/WeeklyForecastViewModel.java
+++ app/src/main/java/com/example/team_7_tcss_450/ui/weather/model/WeeklyForecastViewModel.java
@@ -109,7 +109,7 @@ public class WeeklyForecastViewModel extends AndroidViewModel {
         // URL USES HARDCODED latitude and longitude args, REPLACE ASAP
         // FURTHERMORE, This uses our TEST ENDPOINT, REPLACE WITH PRODUCTION ENDPOINT BEFORE SPRINT MEET
         final String url = getApplication().getResources().getString(R.string.base_url_weather_service) +
-                "forecast?" + "lat=" + location.getLatitude() + "&lon=" + location.getLongitude();
+                "test?" + "lat=" + location.getLatitude() + "&lon=" + location.getLongitude();
 
         final Request<JSONObject> request = new JsonObjectRequest(
                 Request.Method.GET,
